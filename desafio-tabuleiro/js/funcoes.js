473909a66d5e506c18187553a1572e1f880706e1
Stenio Santos <steniocsantos@gmail.com>
2023-02-10 06:19:50 -0300

    função colorir casa

--- desafio-tabuleiro/js/funcoes.js
+++ desafio-tabuleiro/js/funcoes.js
@@ -2,14 +2,23 @@ function criaCasa() {
     const conteudo = document.getElementById("tabuleiro");
 
     let casas = "";
+    let cor = "branca";
 
     for (let index = 1; index <= 64; index++) {
-        casas += '<div class="casa"></div>';
+        casas += `<div class="casa" ${cor}>${index}</div>`;
+
+        cor = alteraCor (cor);
+
+        if (index % 8 == 0) {
+            cor = alteraCor (cor);
+        }
     }
 
-
     conteudo.innerHTML = casas;
+}
 
+function alteraCor(corAtual) {
+    return corAtual == "branca" ? "" : "branca";
 }
 
 criaCasa ();
