9179412d3b6a3bf4ddee72084eb6cd83fa1d2ece
Stenio Santos <steniocsantos@gmail.com>
2023-02-12 13:32:01 -0300

    Tabuleiro Pronto

--- desafio-tabuleiro/js/funcoes.js
+++ desafio-tabuleiro/js/funcoes.js
@@ -3,20 +3,25 @@ function criaCasa() {
 
     let casas = "";
     let cor = "branca";
+   
 
     for (let index = 1; index <= 64; index++) {
-        casas += `<div class="casa" ${cor}>${index}</div>`;
+        casas += `<div class='casa ${cor}'></div>`;
 
-        console.log(cor);
+        cor = alteraCor(cor);
 
-        if (cor % 8 == 0) {
-            cor = corAtual(outraCor );
+        if (index % 8 == 0) {
+            cor = alteraCor(cor);
+
         }
     }
 
     conteudo.innerHTML = casas;
 }
 
-
+function alteraCor (corAtual) {
+    return corAtual == "branca" ? "" : "branca";
+  } 
+    
 
 criaCasa ();
